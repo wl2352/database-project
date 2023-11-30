fd49f4574809a2f1da953353ece8d22d0c2f91ad
Ricky <rzh4321@gmail.com>
2023-11-29 20:26:15 -0500

    finished signup page

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def login():
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port='3000')
