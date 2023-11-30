0b9fed53663c61befb678c622143a42ef184e340
Ricky <rzh4321@gmail.com>
2023-11-29 20:52:00 -0500

    - finished outline of criminal page

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def login():
 def signup():
     return render_template('signup.html')
 
+@app.route('/criminal')
+def criminal_page():
+    return render_template('criminal.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port='3000')
